d18e1c214ce47eb94855b3e8a90dccfe81fabefb
SAIESHAA3 <73050972+SAIESHAA3@users.noreply.github.com>
2021-03-28 13:05:27 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,12 @@
+def swapFileData():
+        f1=input("Enter file name")
+        f2=input("file name")
+        with open(f1,'r') as a :
+            data1=a.read()
+        with open(f2,'r') as b :
+            data2=b.read()
+        with open(f1,'w') as a :
+            a.write(data2)
+        with open(f2,'w') as b :
+            b.write(data1)
+swapFileData()         
